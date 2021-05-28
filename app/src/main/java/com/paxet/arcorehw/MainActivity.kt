3c3cc55ba7016f0b84881d75772e3d10f74c011a
paxett <y.bogdanov@gmail.com>
2021-05-28 13:21:33 +0300

    Sceneform fork used. ARFragment added to main activity.

--- app/src/main/java/com/paxet/arcorehw/MainActivity.kt
+++ app/src/main/java/com/paxet/arcorehw/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.paxet.arcorehw
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.Sceneform
+import com.google.ar.sceneform.ux.ArFragment
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            if(Sceneform.isSupported(this)) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.ar_fragment, ArFragment::class.java, null)
+                    .commit()
+            }
+        }
     }
 }
